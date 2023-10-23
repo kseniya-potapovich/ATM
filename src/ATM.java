9c4c4c75b7d93e6fd80d64ac199f68073d41742f
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-10-23 20:56:13 +0300

    Second commit

--- src/ATM.java
+++ src/ATM.java
@@ -8,23 +8,50 @@ public class ATM {
         this.count20 = count20;
         this.count50 = count50;
         this.count100 = count100;
+        this.allMoney = count100 * 100 + count50 * 50 + count20 * 20;
     }
 
     void addMoney(int x20, int x50, int x100) {
         count20 += x20;
         count50 += x50;
         count100 += x100;
+        allMoney += x100 * 100 + x50 * 50 + x20 * 20;
     }
 
     boolean takeMoney(int summaOfMoney) {
-        if (summaOfMoney > count20 * 20 + count50 * 50 + count100 * 100) {
+        if (allMoney >= summaOfMoney) {
+            System.out.println("Операция выполнена успешно!");
+            int temp = summaOfMoney / 100;
+            if (count100 <= temp) {
+                temp = count100;
+            }
+            summaOfMoney -= temp * 100;
+            allMoney -= temp * 100;
+            count100 -= temp;
+            System.out.println("100: " + temp);
+
+            temp = summaOfMoney / 50;
+            if (count50 <= temp) {
+                temp = count50;
+            }
+            summaOfMoney -= temp * 50;
+            allMoney -= temp * 50;
+            count50 -= temp;
+            System.out.println("50: " + temp);
+
+            temp = summaOfMoney / 20;
+            if (count20 <= temp) {
+                temp = count20;
+            }
+            summaOfMoney -= temp * 20;
+            allMoney -= temp * 20;
+            count20 -= temp;
+            System.out.println("20: " + temp);
+
+            return true;
+        } else {
             System.out.println("Операция не выполнена!" + "\n" + "Недостаточно средств!");
             return false;
-        } else {
-            allMoney = count20 * 20 + count50 * 50 + count100 * 100 - summaOfMoney;
-            System.out.println("Операция выполнена успешно!");
-            System.out.print(summaOfMoney / 100 + "\t" + summaOfMoney % 100 / 50 + "\t" + summaOfMoney % 100 % 50 / 20);
-            return true;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        ATM atm1 = new ATM(4,5,10);
+        ATM atm1 = new ATM(1,2,1);
         atm1.addMoney(1,1,1);
-        atm1.takeMoney(4000);
+        System.out.println(atm1.allMoney);
+        atm1.takeMoney(300);
+       // System.out.println(atm1.count50);
+        System.out.println(atm1.allMoney);
     }
 }
